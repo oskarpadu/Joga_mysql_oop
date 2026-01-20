eec97f6d0065599aa74184536fd98d986cf90058
oskar <oskarpadu31@gmail.com>
2026-01-20 09:44:45 +0200

    Added handlebars template engine

---
+++  index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const path = require('path');
+const mysql = require('mysql');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+const hbs = require('express-handlebars');
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+app.engine('hbs', hbs.engine({ extname: 'hbs', defaultLayout: 'main', layoutsDir: __dirname + '/views/layouts/' }));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const con = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'qwerty',
+    database: 'joga_mysql'
+});
+
+con.connect((err) => {
+    if (err) throw err;
+    console.log('Connected to joga_mysql database!');
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000');
+});
